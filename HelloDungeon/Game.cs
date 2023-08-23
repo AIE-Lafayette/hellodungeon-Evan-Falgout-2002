d040c6984f8d7857e235d39be79f8d2cbabec3da
Evan Falgout <Monkeyjo2002@gmail.com>
2023-08-23 15:05:57 -0500

    Added Some player stats and began to write the intro.

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -10,7 +10,28 @@ namespace HelloDungeon
     {
         public void Run()
         {
+            // Player Stat Numbers
+            float Health = 100.0f;
+            float Defense = 25.0f;
+            int Luck = 1;
+            int Strength= 1;
+            // Player State of Being/Game State
+            bool PlayerAlive = true;
+            
+            // Welcomes Player into game and allows Player to name Character
+            Console.WriteLine("In the land of Ertuum you awake in a haze." +
+                " You slowly come to your senses and notice someone walking towards you." +
+                " You prepare yourself for conflict, However you let your guard down when they stop." +
+                " With a puzzled expression they ask 'Who are You?' ");
+            string name =Console.ReadLine();
+            // Game Cont...
+            Console.WriteLine("Well" + name + "I would not recommend sleeping on the ground my friend." +
+                "My Name is NPC");
+            
 
+
+           
+               
         }
     }
 }
